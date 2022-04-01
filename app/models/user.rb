f06f6e27ccb6d539b3b9fa4a36652b5c345d2433
yama0324 <junya88204@gmail.com>
2022-04-01 14:17:17 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :items
+
          validates :nickname,presence: true
          validates :first_name,presence: true , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,allow_blank: true}
          validates :last_name,presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,allow_blank: true}
