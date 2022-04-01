bbd8c0a542318e7ecf0ba7c0174af922e8235b4f
yama0324 <junya88204@gmail.com>
2022-04-01 14:59:31 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,15 @@
 class ItemsController < ApplicationController
 
   def index
+    
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name,:detail,:category_id,:status_id,:delivery_charge_id,:delivery_area_id,:delivery_date_id,:price,:image).merge(user_id: current_user.id)
+  end
+
 end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :delivery_area
+  belongs_to :delivery_date
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_date_id, numericality: { other_than: 1, message: "can't be blank" }
 end
