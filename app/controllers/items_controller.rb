e83c788a222bb268da4152838d352cec89821401
yama0324 <junya88204@gmail.com>
2022-04-01 15:21:22 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except: [:index]
 
   def index
-    
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
   end
 
   private
