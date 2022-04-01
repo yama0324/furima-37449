2d2b630ebd0b79a4bbad0579facdb70be5c4517e
yama0324 <junya88204@gmail.com>
2022-04-01 15:45:14 +0900

    商品出品ページの表示・修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,15 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+   else
+     render :new
+   end
+  end
+
   private
 
   def item_params
