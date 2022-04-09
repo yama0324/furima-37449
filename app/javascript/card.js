00e45bfb6e051118e4eed81dc69c3eefe6b293b9
yama0324 <junya88204@gmail.com>
2022-04-09 17:54:37 +0900

    トークンの値の表示

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_78d31a26e3a4cea9fca50c59");
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchased_delivery_record[number]"),
+      cvc: formData.get("purchased_delivery_record[cvc]"),
+      exp_month: formData.get("purchased_delivery_record[exp_month]"),
+      exp_year: `20${formData.get("purchased_delivery_record[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
